71ba9db3d23e659df94643cc13b57e607f02df9f
Tom <tom963741@gmail.com>
2023-06-15 01:17:15 +0200

    feat: initialize interface overlay

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Canvas from './components/Canvas/Canvas';
+import Switcher from './components/Switcher/Switcher';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="grid-settings">
+        <label htmlFor="grid-sizeX">Cases sur l'axe X</label>
+        <input type="number" className="grid-param" id="grid-sizeX"/>
+        <label htmlFor="grid-sizeY">Cases sur l'axe Y</label>
+        <input type="number" className="grid-param" id="grid-sizeY"/>
+        <button onClick={() => console.log('reload')}>Reload</button>
+      </div>
+
+      <div className="grid-wrapper">
+        <div className='grid-layout'>
+          <Canvas draw='' height={1000} width={1000} squaresX={10} squaresY={10}></Canvas>
+        </div>
+
+        <div className="grid-instructions">
+          <h3 className="instructions-header">Instructions :</h3>
+          
+          <div className="switch-mode">
+            <span className="mode">Manual</span>
+            <Switcher></Switcher>
+            <span className="mode">Auto</span>
+          </div>
+
+          <div className="instructions-card">
+            <button className="btn btn-action instructions-btn" onClick={() => console.log('droite')}>Droite</button>
+            <button className="btn btn-action instructions-btn" onClick={() => console.log('gauche')}>Gauche</button>
+            <button className="btn btn-action instructions-btn" onClick={() => console.log('avancer')}>Avancer</button>
+          </div>
+
+        </div>
+      </div>
+    
     </div>
   );
 }
