02dfe9204f12c5788bdbe2c011570019703b21f2
Tom <tom963741@gmail.com>
2023-06-15 01:49:47 +0200

    feat: import scss module + first styling interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 import Canvas from './components/Canvas/Canvas';
 import Switcher from './components/Switcher/Switcher';
@@ -9,11 +9,13 @@ function App() {
     <div className="App">
       
       <div className="grid-settings">
-        <label htmlFor="grid-sizeX">Cases sur l'axe X</label>
-        <input type="number" className="grid-param" id="grid-sizeX"/>
-        <label htmlFor="grid-sizeY">Cases sur l'axe Y</label>
-        <input type="number" className="grid-param" id="grid-sizeY"/>
-        <button onClick={() => console.log('reload')}>Reload</button>
+        <label htmlFor="grid-sizeX">Cases sur l'axe X
+          <input type="number" className="grid-param" id="grid-sizeX"/>
+        </label>
+        <label htmlFor="grid-sizeY">Cases sur l'axe Y
+          <input type="number" className="grid-param" id="grid-sizeY"/>
+        </label>
+        <button className='btn btn-action' onClick={() => console.log('reload')}>Reload</button>
       </div>
 
       <div className="grid-wrapper">
